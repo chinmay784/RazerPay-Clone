8d83c799ff5b512e96aa48a29c0f40e1d5df4700
chinmay784 <122338728+chinmay784@users.noreply.github.com>
2023-07-23 09:59:04 +0530

    Add files via upload
    changes

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {fontFamily :{
+      mullish :["Mulish","sans-serif"]
+    },colors :{
+      deepBlue:"#02042e",
+      deep:"#02043a",
+      lightBlue:"#2b84ea",
+      lightBlue300:"#4b94ed",
+      lightBlue500:"#0b71e7",
+      Light:"#00ffcc",
+      grayText:"#818597",
+      lightGray:"#e2e2e2",
+      grayBlue:"#344a6c",
+      deepBlueHead:"#152f56",
+      gray2:"#525a76",
+      blueuuu:"rgb(199, 235, 255)"
+    }},
+  },
+  plugins: [],
+}
+
